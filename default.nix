84e12b8cac88561d18132edd5b7d6e59378ed359
Erven2016 <leiguihua2016@gmail.com>
2024-08-26 15:02:26 +0800

    add(wallpapers)

--- default.nix
+++ default.nix
@@ -18,7 +18,26 @@ let
       format = "png";
       hash = "sha256-AKeaQlven/Y+Jg/AxOCmLH9YlqQJqZj5NM3V29Permo=";
     };
-
+    macos-ventura = {
+      name = "macos-ventura";
+      format = "png";
+      hash = "sha256-PjeUvQEtelbAbCF2UQ/v1Py5/5xJ1Y/UWZYC/8LepRk=";
+    };
+    moon-far-view = {
+      name = "moon-far-view";
+      format = "png";
+      hash = "sha256-m3niNiEy6njnFKCWYrfbEzCXfV+nmd9HPONSzzej3yE=";
+    };
+    win11-bloom-gradient = {
+      name = "win11-bloom-gradient";
+      format = "png";
+      hash = "sha256-nmX2OUXEkewW7gvWvJ8LTsKRiz4mepF0qCnWEh5l9VU=";
+    };
+    win11-bloom-ventura = {
+      name = "win11-bloom-ventura";
+      format = "png";
+      hash = "sha256-SaS5anRQHT/QKREDlmT+sjTb000awnkHF7vpKu0OqWs=";
+    };
   };
 in
 {
@@ -28,4 +47,8 @@ in
   overlays = import ./overlays; # nixpkgs overlays
 
   macos-sonoma = callPackage ./wallpapers/build.nix meta.macos-sonoma;
+  macos-ventura = callPackage ./wallpapers/build.nix meta.macos-ventura;
+  moon-far-view = callPackage ./wallpapers/build.nix meta.moon-far-view;
+  win11-bloom-gradient = callPackage ./wallpapers/build.nix meta.win11-bloom-gradient;
+  win11-bloom-ventura = callPackage ./wallpapers/build.nix meta.win11-bloom-ventura;
 }
